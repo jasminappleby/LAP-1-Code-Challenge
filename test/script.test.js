c4bb3fba8fb2d3199ffc9f971cb6809690941fc1
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-08-03 21:28:48 +0100

    initial commit

---
+++ test/script.test.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@jest/globals')
+const script = require('../client/script')
+
+describe("getListings", () => {
+
+    test("has fetch been called", () => {
+        expect()
+    })
+
+    test("has values been assigned to variables", () => {
+        expect()
+    })
+
+})
+    
