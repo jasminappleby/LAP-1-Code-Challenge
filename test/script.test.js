1d55007500fa3074c0666b2d82d3458eddda18d7
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-08-04 09:50:14 +0100

    before data change

--- test/script.test.js
+++ test/script.test.js
@@ -4,12 +4,14 @@ const script = require('../client/script')
 describe("getListings", () => {
 
     test("has fetch been called", () => {
-        expect()
+        script.getListings();
+        expect(fetch).toHaveBeenCalled();
     })
 
-    test("has values been assigned to variables", () => {
-        expect()
+    test("has fetch returns listings", () => {
+       // let response = JSON.stringify({ id: 1, url: 'https://en.wikipedia.org/wiki/Cheese', pageTitle: 'Cheese - Wikipedia', text: 'Cheese is a dairy product, derived from milk and produced in wide ranges of flavors, textures and forms by coagulation of the milk protein casein.' });
+        //fetchMock.mockResponse = response
+        expect(script.getListings()).toEqual('')
     })
 
 })
-    
